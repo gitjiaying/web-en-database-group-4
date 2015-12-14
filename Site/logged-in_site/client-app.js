3ecd7631f6751fa62947d6ce91733c10b60cd3c6
Mats <M.Mulder-5@student.tudelft.nl>
2015-12-14 10:17:40 +0100

    Tests with server

--- Site/logged-in_site/client-app.js
+++ Site/logged-in_site/client-app.js
@@ -1,1 +1,21 @@
-// JavaScript Document
+var main = function(){
+	"use strict";
+	
+	var addTodoToList = function (todos) {
+		console.log("adding todos from server");
+		var todolist = document.getElementById("todolists");
+		for (var key in todos) {
+			var li = document.createElement("li");
+			li.innerHTML = "TODO: " + todos[key].message;
+			todolist.appendChild(li);
+		}
+	};
+	
+	$.getJSON("../todos", addTodoToList)
+	.error(function (jqXHR, textStatus, errorThrown){
+		console.log("error " + textStatus);
+		console.log("incoming Text " + jqXHR.responseText);
+	});
+		
+};
+$(document).ready(main);
