544ed7880e656279918f45c679ad378aebc673b8
JY <jiaying.hu@hotmail.com>
2015-12-15 18:31:12 +0100

    todo

--- Assignment 2/logged-in.js
+++ Assignment 2/logged-in.js
@@ -5,7 +5,7 @@ function TodoList () {
 	this.status = function () {
 		return this.countDone + "/" + this.getNumberTodos();
 	}; //initially 0/0 todos done
-	this.list = []; //empty list
+	this.todos = []; //empty list
 	this.kind = task; //note or tasklist
 	this.countDone = 0; //number of todos in this list done
 };
@@ -32,7 +32,7 @@ TodoList.prototype.setPriority = function(priority) {
 };
 TodoList.prototype.getPriority = function() {
 	return this.priority;
-}
+};
 TodoList.prototype.getNumberTodos = function () {
 	return this.numberTodos;
 };
@@ -48,9 +48,9 @@ function Todo () {
 	this.duedate = "today";
 	this.duetime = "now"
 	this.label = "low";
-	this.todoListclass = "";
-	this.status = false;
-	this.creationDate = "today";
+	this.todoListclass = null;
+	this.status = "undone";
+	this.creationDate = new Date().toUTCString();
 }
 
 Todo.prototype.setContent = function (content) {
@@ -59,16 +59,19 @@ Todo.prototype.setContent = function (content) {
 Todo.prototype.setDueDate = function (duedate) {
 	this.duedate = duedate;
 };
+// Todo.prototype.creationDueDate = function () {
+// 	var today = new Date(); //creates a new date, default is the creationdate
+// 	this.creationDate = today.toUTCString();
+// };
 Todo.prototype.setLabel = function (label) { //medium as default
 	this.label = label;
 };
 Todo.prototype.setStatus = function (status) {
 	this.status = status;
 };
-
 ToDo.prototype.setDueTime = function(time) {
 	this.duetime = time;
-}
+};
 
 
 
@@ -86,15 +89,15 @@ function addTaskList() {
 	//var k = todolists.push(newTodolist); //when pushed, retrieve array[i] to change the members
 	var $new_todolist = $("<li>"); //jquery to append to the DOM, create a new list element with a nested unordered list
 	$('#todolists').append($new_todolist);
-	UpdateTodoList($new_todolist, todolists.length); //call the update 
+	// UpdateTodoList($new_todolist, todolists.length); //call the update 
 	
-	function UpdateTodoList (todoList, index) { //setAttribute replaces the values of the attributes, so need to remove and add values
-	$("#todolists").children('li').setAttribute('class', function(n) { //give all the todolists in the ul #todolists the classes  todolist and their number n in the DOM
-            return "todolist " + n; //this will give the todolists a number in the array
-	});
-	//todoList.setAttribute('title', todolists[index].getName());
-	todoList.setAttribute('data-label', todoList[index].getPriority());
-	};
+	// function UpdateTodoList (todoList, index) { //setAttribute replaces the values of the attributes, so need to remove and add values
+	// 	$("#todolists").children('li').setAttribute('class', function(n) { //give all the todolists in the ul #todolists the classes  todolist and their number n in the DOM
+	//             return "todolist " + n; //this will give the todolists a number in the array
+	// 	});
+	// 	//todoList.setAttribute('title', todolists[index].getName());
+	// 	todoList.setAttribute('data-label', todoList[index].getPriority());
+	// };
 	
 	var $title = $('<input type="text" name = "title" size ="10" class="todolist-header-title" value="">'); 
 	$new_todolist.append($title);
@@ -119,12 +122,20 @@ function addTaskList() {
 	};
 
 	var k = todolists.push(newTodolist); //push at the end 
+	UpdateTodoList($new_todolist, todolists.length); //call the update 
+	
+	function UpdateTodoList (todoList, index) { //setAttribute replaces the values of the attributes, so need to remove and add values
+		todolist.setAttribute('class','todolist ' + index-1 );
+		todoList.setAttribute('data-label', todoList[index].getPriority());
+	};
+
+
 	//$("#todolists").append('<li> <ul class="todolist_"  data-label = "low"> <p> Container for todolist</p> <button onClick="addTodo()">+</button> </ul> </li>');
 };
 
 function addNoteList() {
 
-}
+};
 
 function addList() {
 	//show the buttons, can also put the buttons in a <div> and let the that fade
@@ -146,9 +157,20 @@ function addList() {
 $('#addList-button').on('click', addList);
 
 //toggle menu-section
+$('#menu-section').hide();
+
 function MenuFade(menu, event) {
-$(this).fadeToggle(); //of toggle() of slideToggle()
+	$(this).fadeToggle(); //of toggle() of slideToggle()
+	var src1 = "elements/Menu-button.png";
+	var src2 = "elements/Menu-button_close.png";
+	if($("#menu-button").attr("src") === src1){
+		$("#menu-button").attr("src",src2);
+	}
+	else{
+		$("#menu-button").attr("src",src1);
+		};
 };
+
 
 $('#menu-section').on('click', MenuFade(menu, event));
 
@@ -169,14 +191,53 @@ $('.todo').focus(Label);
 //make bold, italic, underlined selected text
 function Style(style) { //document.execCommand is already a toggler itself, use this method on selections in the document, you have to mke sure the content is editable though
 	document.execCommand(style, false, null); //document = javascript whole document, here it takes the selection
-}
+};
 
 $("#bold").on('click', Style("bold"));
 $("#italic").on('click', Style("italic"));
 $("#underlined").on('click', Style("underline"));
 
 //add todo
+function addTodo() {
+	var newTodo = new ToDo();
 
+	var $new_todo = $('<li>');
+	var thisList = $(this).parent().attr('class').split(' ')[1]); //get index of the todoList in the todolists-array
+	
+	todolists[thisList -1].todos.push(newTodo);
+	var n = todolists[thisList-1].todos.length;
+	$new_todo.addClass('todo ' + n + ' todoIn ' + thisList);
+	$new_todo.setAttribute('data-label', 'low' );
+	$(this).parent().append($new_todo);
+
+	var $content = $('<input type="text">');
+	$content.setAttribute('class', 'todo-content');
+	$new_todo.append($content);
+
+	var $status = $('<input>');
+	$status.setAttribute('class', 'undone');
+	$new_todo.append($status);
+
+	var $dueDate = $('<input>');
+	$duedate.setAttribute("placeholder", "set duedate");
+	$new_todo.append($duedate);
+
+};
+
+$('.addTodo').on('click', addTodo());
 
 };
 $(document).ready(main);
+
+
+
+
+
+
+
+
+
+
+
+
+
